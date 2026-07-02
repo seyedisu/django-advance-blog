3d00e7bc5334be03ee5034be690624abb34d710f
seyedisu <this.is.seyed.parsa@gmail.com>
2026-07-02 22:18:56 +0330

    Config statics

--- core/core/settings.py
+++ core/core/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "meedia"
 
 # user manager config
 AUTH_USER_MODEL = "accounts.User"
