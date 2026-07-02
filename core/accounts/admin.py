48d3f25a0e9610aa1ecb7a5f7151b8fc5525c24b
seyedisu <this.is.seyed.parsa@gmail.com>
2026-07-02 20:04:24 +0330

    Create Admin Class: CustomUserAdmin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User
 
 # Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "is_superuser", "is_active")
+    list_filter = ("email", "is_superuser", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = [
+        ("Authentication", {
+            "fields": (
+                "email",
+                "password"
+            )
+        }
+        ),
+        ("permissions", {
+            "fields": (
+                ("is_staff", "is_active", "is_superuser"),
+            )
+        }
+        ),
+        ("group permissions", {
+            "fields": (
+                "groups",
+                "user_permissions"
+            )
+        }
+        ),
+        ("important date", {
+            "fields": (
+                "last_login",
+            )
+        }
+        ),
+    ]
+
+    add_fieldsets = [
+        (None, {
+            "fields":(
+                "email",
+                "password1",
+                "password2",
+                ("is_staff", "is_active", "is_superuser")
+            )
+        }
+        ),
+    ]
